217973d7b474c76e4b646a3f78f921506c9e0249
Blackmon <xmiblackmon@davidson.edu@jupyterctl0.davidson.edu>
2020-04-17 14:03:33 -0400

    add prelim Davidson integration

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -1,38 +1,47 @@
 import os
 import shlex
 import tempfile
-
+from jupyterhub.utils import random_port
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
+# pull in Davidson's JupyterHub environment variables 
+HUB_PATH = os.environ.get('HUB_PATH', '')
+HUB_PRIVATE = os.environ.get('HUB_PRIVATE', '')
 
 def setup_desktop():
     # make a secure temporary directory for sockets
     # This is only readable, writeable & searchable by our uid
-    sockets_dir = tempfile.mkdtemp()
+
+    # XXX: the following need to be node-local
+    sockets_dir = tempfile.mkdtemp() 
     sockets_path = os.path.join(sockets_dir, 'vnc-socket')
 
     vnc_command = ' '.join((shlex.quote(p) for p in [
         os.path.join(HERE, 'share/tigervnc/bin/vncserver'),
         '-verbose',
-        '-xstartup', os.path.join(HERE, 'share/xstartup'),
-        '-geometry', '1680x1050',
+        '-xstartup', os.path.join(HUB_PRIVATE, 'xstartup'),
+        '-geometry', '1280x720',
         '-SecurityTypes', 'None',
         '-rfbunixpath', sockets_path,
         '-fg',
-        ':1',
+        # XXX: quick hack to enable multi. users
+        ':'+str(min([ii for ii in range(1,7) \
+                     if not os.path.exists(f'/tmp/.X11-unix/X{ii}')])),
     ]))
+    port = random_port()
     return {
         'command': [
             'websockify', '-v',
             '--web', os.path.join(HERE, 'share/web/noVNC-1.1.0'),
             '--heartbeat', '30',
-            '5901',
+            f'{port}',
             '--unix-target', sockets_path,
             '--',
             '/bin/sh', '-c',
             f'cd {os.getcwd()} && {vnc_command}'
         ],
-        'port': 5901,
+        'port': port,
         'timeout': 30,
         'mappath': {'/': '/vnc_lite.html'},
     }
