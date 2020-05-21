32da4dcf6b8ecc991c4efef650d29e41e5062590
Michael Blackmon <miblackmon@davidson.edu>
2020-05-21 04:21:02 -0400

    update service def

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -8,6 +8,8 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 # import some enironment variables 
 HOME = os.environ.get('HOME')
 
+VNC_PATH = '/usr'
+
 # pull in Davidson's JupyterHub environment variables 
 HUB_PATH = os.environ.get('HUB_PATH', '')
 HUB_PRIVATE = os.environ.get('HUB_PRIVATE', HOME)
@@ -21,13 +23,15 @@ def setup_desktop():
     sockets_path = os.path.join(sockets_dir, 'vnc-socket')
 
     vnc_command = ' '.join((shlex.quote(p) for p in [
-        os.path.join(HERE, 'share/tigervnc/bin/vncserver'),
+        os.path.join(VNC_PATH, 'bin/vncserver'),
         '-verbose',
         '-xstartup', os.path.join(HUB_PRIVATE, 'xstartup'),
         '-geometry', '1024x768',
         '-SecurityTypes', 'None',
         '-rfbunixpath', sockets_path,
         '-fg',
+        '-ZlibLevel', '4',
+        '-depth', '16',
         '-UseIPv4=0',
         '-UseIPv6=0',
         '-auth', os.path.join(HOME,'.Xauthority'),
