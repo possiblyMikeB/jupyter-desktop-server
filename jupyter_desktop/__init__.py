613a28ee22b483ea1f403ce76a43c37297ee6fcb
possiblyMikeB <58000192+possiblyMikeB@users.noreply.github.com>
2020-06-20 16:33:22 -0400

    hedging toward jupyter-server-proxy 1.4

--- jupyter_desktop/__init__.py
+++ jupyter_desktop/__init__.py
@@ -57,5 +57,7 @@ def setup_desktop():
         'timeout': 30,
         'mappath': {'/': '/vnc_lite.html'},
         'launcher_entry': { 'enabled': True,
-                            'title': 'Desktop' },
+                            'title': 'Desktop' }#,
+        #'new_browser_window': True
+
     }
